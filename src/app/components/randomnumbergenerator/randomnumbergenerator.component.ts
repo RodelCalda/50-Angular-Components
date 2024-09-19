18c84c8ad341318f9e2900afe515a56916695f49
Rodel Calda <rodelcalda2@gmail.com>
2024-09-19 12:39:32 -0400

    randomnumbergenerator

---
+++ src/app/components/randomnumbergenerator/randomnumbergenerator.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomnumbergenerator',
+  templateUrl: './randomnumbergenerator.component.html',
+  styleUrls: ['./randomnumbergenerator.component.css']
+})
+export class RandomnumbergeneratorComponent {
+  min: number = 1;
+  max: number = 100;
+  randomNumber: number | null = null;
+
+  generateRandomNumber() {
+    if (this.min < this.max) {
+      this.randomNumber = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    } else {
+      this.randomNumber = null;
+      alert('Minimum value must be less than maximum value.');
+    }
+  }
+}
+
